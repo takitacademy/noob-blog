a832c51dea6ead363aa6677bfb2daf517391290e
knowbee <knowbeeinc@gmail.com>
2021-04-25 18:30:15 +0200

    show all posts and a single post by its id

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
+// initialize global variable to keep track of all posts
+let data
 
-
-const getPost = async () =>{
-
+const getPost = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const data = await response.json();
-    console.log(data)
-
+    data = await response.json();
     let html = "";
-    data.filter(val => val.id<= 12).forEach( post => {
-        html +=`
-        <a href="post.html" class="post">
+    data.filter(val => val.id <= 12).forEach(post => {
+        html += `
+        <a href="#" class="post" onclick="viewPost(${post.id})"> 
             <img src="img/a.jpg" alt="post">
             <p style = "font-style:italic">Maret 01, 2021</p>
             <h3>${post.title}</h3>
@@ -17,42 +15,15 @@ const getPost = async () =>{
         </a>
         `;
         document.getElementById('getRequest').innerHTML = html;
-    }); 
-} 
+    });
+}
 getPost();
 
-const postDisplay = async () =>{
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1');
-    const result = await res.json();
-    console.log(result) 
-
-    let html1 = "";
-    result.forEach(val => {
-        html1 += `
-        <div id="blogPost" class="blog-details">
-            <img src="img/d.jpg" alt="image">
-            <h3>${val.title}</h3>
-            <p>${val.body}</p>
-        </div>`;
-    });
-    
-    document.getElementById("blogPost").innerHTML = html1;
+const viewPost = (id) => {
+    // get post by id
+    let currentPost = data.filter((post) => post.id == id)[0]
+    // save the post in the localstorage
+    localStorage.setItem("currentPost", JSON.stringify(currentPost))
+    // redirect the user to the post page
+    window.location.href = `${location.pathname}post.html`
 }
-postDisplay() 
-
-const comments = async () => {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
-    const resul = await resp.json();
-    console.log(resul);
-
-    let html2 = "";
-    resul.forEach(valu => {
-        html2 += `
-        <div class="blog-details">
-            <h3>${valu.name}</h3>
-            <p>${valu.body}</p>
-        </div>`;
-    });
-    document.getElementById('comments').innerHTML = html2
-}
-comments();
