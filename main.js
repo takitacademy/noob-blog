f1f939aeb50cfa2bdb50939ef0178d9e0abcccd3
NoobMaester <NoobMaester@github.com>
2021-05-07 17:37:28 +0200

    full screen added

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
 let data
-
 const getPost = async () =>{
 
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch("./posts.JSON");
     const data = await response.json();
-    console.log(data)
 
     let html = "";
-    data.filter(val => val.id <= 12).forEach( post => {
+    data.filter(post => post.id <= 12).forEach( post => {
         html +=`
-        <a href="post.html" class="post">
+        <div class="post">
             <img src="img/a.jpg" alt="post">
             <p style = "font-style:italic">Maret 01, 2021</p>
             <h3>${post.title}</h3>
             <p>${post.body}</p>
-        </a>
+        </div>
         `;
         document.getElementById('getRequest').innerHTML = html;
-    }); 
-} 
+    });
+
+    currentPost = data.filter(value => value.id)[1];
+    const post = document.getElementById('getRequest');
+    post.addEventListener('click', viewpost);
+
+    console.log(currentPost)
+}
 getPost();
 
-const viewPost = (id) => {
+const viewpost = () =>{
+    console.log('clicked')
+    localStorage.setItem('currentPost', JSON.stringify(currentPost));
+    //location.pathname = 'post.html';
+    //window.location.href = `${location.pathname}post.html`;
+    window.location.href = 'post.html';
+}
+
+
+/*const viewPost = (id) => {
     //get post by id
     let currentPost = data.filter((post) => post.id) [0];
     
@@ -33,3 +46,5 @@ const viewPost = (id) => {
 
     console.log(currentPost)
 }
+*/
+
--- post.js
+++ post.js
@@ -1,14 +1,14 @@
 const comments = async () => {
     const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/1/comments`);
 
-    const resul = await resp.json();
+    const result = await resp.json();
 
     let html2 = "";
-    resul.forEach(valu => {
+    result.forEach(val => {
         html2 += `
         <div class="blog-details">
-            <h3>${valu.name}</h3>
-            <p>${valu.body}</p>
+            <h3>${val.name}</h3>
+            <p>${val.body}</p>
         </div>`;
     });
     document.getElementById('comments').innerHTML = html2
@@ -17,16 +17,19 @@ comments();
 
 const postDisplay = async () => {
     //retrieve the current post from local storage
-    let post = JSON.parse(localStorage.getItem("currentPost"));
-    //fetch comments by post id
+    retrieved = localStorage.getItem('currentPost');
+    post = JSON.parse(retrieved);
+    console.log(post)
 
+    //fetch comments by post id
     let html1 = "";
-        html1 += `
-        <div id="blogPost" class="blog-details">
-            <img src="img/d.jpg" alt="image">
-            <h3>${val.title}</h3>
-            <p>${val.body}</p>
-        </div>`;
+    html1 += `
+    <div id="blogPost" class="blog-details">
+        <img src="img/d.jpg" alt="image">
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+    </div>`;
+    
         
     document.getElementById("blogPost").innerHTML = html1;
 }
